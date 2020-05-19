3b9fec1f301fff4b47419efed754a9284552a156
unknown <hoge@example.com>
2020-05-20 00:59:33 +0900

    output html

--- ramen.py
+++ ramen.py
@@ -281,4 +281,6 @@ class Tabelog:
 
 tokyo_ramen_review = Tabelog(base_url="https://tabelog.com/tokyo/rstLst/ramen/", test_mode=False, p_ward='東京都内')
 # CSV保存
-tokyo_ramen_review.df.to_csv("./tokyo_ramen_review.csv")
+# tokyo_ramen_review.df.to_csv("./tokyo_ramen_review.csv")
+# HTML保存
+tokyo_ramen_review.df.to_html("./index.html")
